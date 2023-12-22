9e5a155cf4535ca658de975f21234707db990fb7
cherhy <ekxk1234@naver.com>
2023-12-22 22:54:25 +0900

    create test

---
+++ src/main/kotlin/com/example/fastcampuspay/FastcampusPayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fastcampuspay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FastcampusPayApplication
+
+fun main(args: Array<String>) {
+    runApplication<FastcampusPayApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/fastcampuspay/FastcampusPayApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.example.fastcampuspay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FastcampusPayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
