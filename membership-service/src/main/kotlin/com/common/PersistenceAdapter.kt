2158a9297f1543cbf8e13ec8ed67820ec8338d50
cherhy <ekxk1234@naver.com>
2023-12-23 23:59:22 +0900

    create annotation

---
+++ membership-service/src/main/kotlin/com/common/PersistenceAdapter.kt
@@ -0,0 +1,12 @@
+package com.common
+
+import org.springframework.stereotype.Component
+import java.lang.annotation.*
+import java.lang.annotation.Retention
+import java.lang.annotation.Target
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+annotation class PersistenceAdapter()
