a477675775a61bbf122780f97e0d3ebaa62be914
cherhy <ekxk1234@naver.com>
2023-12-24 02:14:02 +0900

    create port

---
+++ membership-service/src/main/kotlin/com/pay/membership/application/port/out/RegisterMembershipPort.kt
@@ -0,0 +1,19 @@
+package com.pay.membership.application.port.out
+
+import com.pay.membership.domain.Membership.Companion.MembershipAddress
+import com.pay.membership.domain.Membership.Companion.MembershipEmail
+import com.pay.membership.domain.Membership.Companion.MembershipId
+import com.pay.membership.domain.Membership.Companion.MembershipIsCorp
+import com.pay.membership.domain.Membership.Companion.MembershipIsValid
+import com.pay.membership.domain.Membership.Companion.MembershipName
+
+interface RegisterMembershipPort {
+    fun createMembership(
+        membershipId: MembershipId,
+        membershipName: MembershipName,
+        membershipEmail: MembershipEmail,
+        membershipAddress: MembershipAddress,
+        membershipIsValid: MembershipIsValid,
+        membershipIsCorp: MembershipIsCorp,
+    )
+}
