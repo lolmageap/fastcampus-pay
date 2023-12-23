0b8a09de987c7109791f51bfcf9e711ff24d9854
cherhy <ekxk1234@naver.com>
2023-12-24 02:11:12 +0900

    add inner classes and factory method

--- membership-service/src/main/kotlin/com/pay/membership/domain/Membership.kt
+++ membership-service/src/main/kotlin/com/pay/membership/domain/Membership.kt
@@ -1,13 +1,6 @@
 package com.pay.membership.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-
-@Entity
-class Membership(
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
+class Membership private constructor(
     val id: String,
     val name: String,
     val email: String,
@@ -15,5 +8,69 @@ class Membership(
     val isValid: Boolean,
     val isCorp: Boolean,
 ) {
+    companion object {
+        fun generateMembership(
+            membershipId: MembershipId,
+            membershipName: MembershipName,
+            membershipEmail: MembershipEmail,
+            membershipAddress: MembershipAddress,
+            membershipIsValid: MembershipIsValid,
+            membershipIsCorp: MembershipIsCorp,
+        ): Membership = Membership(
+            id = membershipId.id,
+            name = membershipName.name,
+            email = membershipEmail.email,
+            address = membershipAddress.address,
+            isValid = membershipIsValid.isValid,
+            isCorp = membershipIsCorp.isCorp,
+        )
 
+        class MembershipId {
+            fun membershipId(value: String) {
+                this.id = value
+            }
+
+            lateinit var id: String
+        }
+
+        class MembershipName {
+            fun membershipName(value: String) {
+                this.name = value
+            }
+
+            lateinit var name: String
+        }
+
+        class MembershipEmail {
+            fun membershipEmail(value: String) {
+                this.email = value
+            }
+
+            lateinit var email: String
+        }
+
+        class MembershipAddress {
+            fun membershipAddress(value: String) {
+                this.address = value
+            }
+
+            lateinit var address: String
+        }
+
+        class MembershipIsValid {
+            fun membershipIsValid(value: Boolean) {
+                this.isValid = value
+            }
+
+            var isValid: Boolean = false
+        }
+
+        class MembershipIsCorp {
+            fun membershipIsCorp(value: Boolean) {
+                this.isCorp = value
+            }
+
+            var isCorp: Boolean = false
+        }
+    }
 }
