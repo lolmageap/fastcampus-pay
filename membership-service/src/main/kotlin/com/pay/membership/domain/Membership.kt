a1d8d9a07335372b851f779d2396c0baedfcb481
cherhy <ekxk1234@naver.com>
2023-12-23 13:00:35 +0900

    create membership entity

--- membership-service/src/main/kotlin/com/pay/membership/domain/Membership.kt
+++ membership-service/src/main/kotlin/com/pay/membership/domain/Membership.kt
@@ -1,9 +1,13 @@
 package com.pay.membership.domain
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 
 @Entity
 class Membership(
+    @Id @GeneratedValue(strategy = GenerationType.UUID)
     val id: String,
     val name: String,
     val email: String,
