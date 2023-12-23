f42286da17b44bfb1f87d3de1ca40dfeb245896b
cherhy <ekxk1234@naver.com>
2023-12-24 02:13:47 +0900

    fix endpoint to controller

--- membership-service/src/main/kotlin/com/pay/membership/adapter/in/web/RegisterMembershipController.kt
+++ membership-service/src/main/kotlin/com/pay/membership/adapter/in/web/RegisterMembershipController.kt
@@ -1,14 +1,37 @@
 package com.pay.membership.adapter.`in`.web
 
 import com.common.WebAdapter
-import org.springframework.web.bind.annotation.GetMapping
+import com.pay.membership.application.port.`in`.RegisterMembershipCommand
+import com.pay.membership.application.port.`in`.RegisterMembershipUseCase
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @WebAdapter
 @RestController
-class RegisterMembershipController {
+class RegisterMembershipController(
+    private val registerMembershipUseCase: RegisterMembershipUseCase,
+) {
 
-    @GetMapping("/test")
-    fun test() = "hello world"
+    @PostMapping("/membership")
+    fun registerMembership(
+        @RequestBody request: RegisterMembershipRequest,
+    ) {
+        // TODO : 1. request
+
+        // TODO : 2. request -> command
+
+        // TODO : 3. use case ~~
+
+        val command = RegisterMembershipCommand(
+            name = request.name,
+            address = request.address,
+            email = request.email,
+            isValid = true,
+            isCorp = request.isCorp,
+        )
+
+        registerMembershipUseCase.registerMembership(command)
+    }
 
 }
