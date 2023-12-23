77fb458497e3d8f49aa34df0bdc7b32458719344
cherhy <ekxk1234@naver.com>
2023-12-23 23:59:37 +0900

    add annotaion

--- membership-service/src/main/kotlin/com/pay/membership/adapter/in/web/RegisterMembershipController.kt
+++ membership-service/src/main/kotlin/com/pay/membership/adapter/in/web/RegisterMembershipController.kt
@@ -1,8 +1,10 @@
-package com.pay.membership
+package com.pay.membership.adapter.`in`.web
 
+import com.common.WebAdapter
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+@WebAdapter
 @RestController
 class RegisterMembershipController {
 
