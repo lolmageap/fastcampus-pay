21eefdbf5664d6e725bda93771537fc78a88d38f
lolmageap <ekxk1234@naver.com>
2023-12-24 19:06:40 +0900

    create service

---
+++ membership-service/src/main/kotlin/com/pay/membership/application/service/FindMembershipService.kt
@@ -0,0 +1,25 @@
+package com.pay.membership.application.service
+
+import com.pay.membership.adapter.out.persistence.MembershipMapper
+import com.pay.membership.application.port.`in`.FindMembershipCommand
+import com.pay.membership.application.port.`in`.FindMembershipUseCase
+import com.pay.membership.application.port.out.FindMembershipPort
+import com.pay.membership.domain.Membership
+import com.pay.membership.domain.Membership.Companion.MembershipId
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional
+class FindMembershipService(
+    private val findMembershipPort: FindMembershipPort,
+    private val membershipMapper: MembershipMapper,
+): FindMembershipUseCase {
+    override fun findMembership(command: FindMembershipCommand): Membership {
+        val membershipEntity = findMembershipPort.findMembership(
+            MembershipId(command.membershipId)
+        )
+        return membershipMapper.mapToDomainEntity(membershipEntity)
+    }
+
+}
--- membership-service/src/main/kotlin/com/pay/membership/application/service/RegisterMembershipService.kt
+++ membership-service/src/main/kotlin/com/pay/membership/application/service/RegisterMembershipService.kt
@@ -1,14 +1,30 @@
 package com.pay.membership.application.service
 
+import com.pay.membership.adapter.out.persistence.MembershipMapper
 import com.pay.membership.application.port.`in`.RegisterMembershipCommand
 import com.pay.membership.application.port.`in`.RegisterMembershipUseCase
+import com.pay.membership.application.port.out.RegisterMembershipPort
 import com.pay.membership.domain.Membership
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class RegisterMembershipService: RegisterMembershipUseCase {
+@Transactional
+class RegisterMembershipService(
+    private val registerMembershipPort: RegisterMembershipPort,
+    private val membershipMapper: MembershipMapper,
+): RegisterMembershipUseCase {
     override fun registerMembership(command: RegisterMembershipCommand): Membership {
-        TODO("Not yet implemented")
+
+        val membershipJpaEntity = registerMembershipPort.createMembership(
+            Membership.Companion.MembershipName(command.name),
+            Membership.Companion.MembershipEmail(command.email),
+            Membership.Companion.MembershipAddress(command.address),
+            Membership.Companion.MembershipIsValid(command.isValid),
+            Membership.Companion.MembershipIsCorp(command.isCorp),
+        )
+
+        return membershipMapper.mapToDomainEntity(membershipJpaEntity)
     }
 
 }
