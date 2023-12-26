98010f5bb82f2aa9a548617db8ef5f2e993fbb71
cherhy <ekxk1234@naver.com>
2023-12-26 22:38:20 +0900

    add submodules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,7 @@
 rootProject.name = "fastcampusPay"
 include("membership-service")
+include("money-service")
+include("payment-service")
+include("remittance-service")
+include("settlement-service")
+include("banking-service")
