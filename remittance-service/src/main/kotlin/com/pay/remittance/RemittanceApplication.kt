89098db173e20b0ebda1603b6986b14b48de2320
cherhy <ekxk1234@naver.com>
2023-12-26 22:37:56 +0900

    create remittance module

---
+++ remittance-service/src/main/kotlin/com/pay/remittance/RemittanceApplication.kt
@@ -0,0 +1,11 @@
+package com.pay.remittance
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RemittanceApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoneyApplication>(*args)
+}
