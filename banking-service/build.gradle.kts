ffee51a5e19a992ecf6d410224be81365cdb8aba
cherhy <ekxk1234@naver.com>
2023-12-27 23:28:16 +0900

    fix language kotlin to banking

--- banking-service/build.gradle.kts
+++ banking-service/build.gradle.kts
@@ -1,17 +1,7 @@
-plugins {
-    id("java")
-}
-
-group = "com.payment"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
 dependencies {
-}
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+    runtimeOnly("com.h2database:h2")
 }
