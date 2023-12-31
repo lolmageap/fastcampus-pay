c49d76e26c4ab515ed6ff2c20f1cfc8729be91ee
cherhy <ekxk1234@naver.com>
2023-12-31 22:59:34 +0900

    add kotest and mockk dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,15 @@ subprojects {
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+        // test
+        testImplementation("com.h2database:h2")
+        testImplementation("com.ninja-squad:springmockk:4.0.2")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+        testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+        testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+        testImplementation("io.kotest.extensions:kotest-extensions-testcontainers:2.0.2")
     }
 
     dependencyManagement {
